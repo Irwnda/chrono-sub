b8f8a2f534093a9a92cabe0affd088c1891a7b17
irwanda <irwandaa7@gmail.com>
2026-06-28 20:58:00 +0700

    feat: display subtitle path

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use chrono_sub::{io, subtitle};
+use crossterm::style::{style, Color, Stylize};
 use std::process;
 
 fn main() {
     match io::start() {
         Ok(file) => {
+            println!("Selected file: {}", style(file.display()).with(Color::Green).bold());
+
             subtitle::process_file(file);
         },
         Err(err) => {
